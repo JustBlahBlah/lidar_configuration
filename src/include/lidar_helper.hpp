3e7cedf24c3f448596fadcfa36f36d1e40c0de99
Yu <realhikkiboy@gmail.com>
2025-12-14 15:09:43 +0000

    Add lidar helper functions

---
+++ src/include/lidar_helper.hpp
@@ -0,0 +1,10 @@
+#ifndef LIDAR_HELPER
+#define LIDAR_HELPER
+#include <defines.hpp>
+
+uint8_t calculate_checksum(const std::vector<uint8_t>& cmd);
+int setup_serial(int fd, int baud_rate);
+bool check_strict_connection(int fd);
+void send_command(int fd, std::string name, std::vector<uint8_t> cmd);
+
+#endif LIDAR_HELPER
